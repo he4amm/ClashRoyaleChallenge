7acd2a43c0ee4523a8d7492e2daf56743e703cee
Hesham Mamdouh <he4amm@gmail.com>
2018-05-06 14:14:06 +0200

    [add] preload functionality to cards' image

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -8,6 +8,9 @@ import { Card } from './../card';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  urlImage: string;
+  tmpImg: HTMLImageElement;
+
   @Input('card') card: Card;
   @Input('cardDetailsActivated') cardDetailsActivated: string;
   @Output() activateCardDetails: EventEmitter<string> = new EventEmitter<string>();
@@ -17,14 +20,29 @@ export class CardComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.preLoadImage(this.card, (image) => {
+      this.urlImage = image;
+    });
+  }
+
+  showCardDetails(id: string) {
+    this.activateCardDetails.next(id);
+  }
+
+  preLoadImage(card: Card, callback) {
+    callback('https://k61.kn3.net/taringa/1/5/6/4/3/3/34/adriano034/944.gif');
+
+    const loaded = () => {
+      callback(this.getImageUrl(card));
+    };
+
+    this.tmpImg = new Image();
+    this.tmpImg.onload = loaded;
+    this.tmpImg.src = this.getImageUrl(card);
   }
 
   getImageUrl(card: Card) {
     return this.dataService.baseUrl + `/images/cards/${card.idName}.png`;
-  }
-
-  showCardDetails( id: string ) {
-    this.activateCardDetails.next(id);
   }
 
 }
