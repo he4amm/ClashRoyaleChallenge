80f3eed48635c0a02dbf962ff42a1e77a8315798
Hesham Mamdouh <he4amm@gmail.com>
2018-05-06 05:18:42 +0200

    [add] main three components with their styles (body, foooter, header)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppBodyComponent } from './app-body/app-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    AppBodyComponent
   ],
   imports: [
     BrowserModule
